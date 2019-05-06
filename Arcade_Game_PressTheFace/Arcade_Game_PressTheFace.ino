ebc94694a13629c15bf24c63b79bf18c1bced486
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-05-06 21:03:56 +0200

    Metodo Start() e inizio metordo Gioco()

--- Arcade_Game_PressTheFace/Arcade_Game_PressTheFace.ino
+++ Arcade_Game_PressTheFace/Arcade_Game_PressTheFace.ino
@@ -1,10 +1,12 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
+int Stato;
+int x = 500;
 const int buttonPin1 = 22;
 const int buttonPin2 = 24;
 const int buttonPin3 = 26;
+
 
 byte Smile[8] = {
   B00000,
@@ -79,25 +81,25 @@ void BrokenHeartF() {
   lcd.createChar(2, BrokenHeart);
   lcd.setCursor(1, 0);
   lcd.write(byte(2));
-  delay(400);
+  delay(300);
 }
 void Broken2HeartF() {
   lcd.createChar(3, Broken2Heart);
   lcd.setCursor(1, 0);
   lcd.write(byte(3));
-  delay(400);
+  delay(300);
 
 }
 void DeathHeartF() {
   lcd.createChar(4, DeathHeart);
   lcd.setCursor(1, 0);
   lcd.write(byte(4));
-  delay(400);
+  delay(300);
 
 }
 void Fullhealth() {
-  lcd.createChar(1,Heart);
-  lcd.setCursor(13,0);
+  lcd.createChar(1, Heart);
+  lcd.setCursor(13, 0);
   lcd.print(char(1));
   lcd.print(char(1));
   lcd.print(char(1));
@@ -107,15 +109,46 @@ void Evilface() {
   lcd.setCursor(4, 0);
   lcd.print(char(0));
 }
-
-void setup() {
-  lcd.begin(16,2);
-   pinMode(buttonPin1, INPUT_PULLUP);
-   pinMode(buttonPin2, INPUT_PULLUP);
-   pinMode(buttonPin3, INPUT_PULLUP);
-   lcd.setCursor(2,0);lcd.print("Benvenuto in");lcd.setCursor(1,1);
-   lcd.print("Press the Face");delay(3000);lcd.clear();
-   
+void BottoniDif() 
+{
+  lcd.clear();
+  lcd.print("Easy Normal Hard");
+  while (digitalRead (buttonPin1)!= LOW && digitalRead (buttonPin2)!= LOW && digitalRead (buttonPin3) != LOW) {
+    if (digitalRead(buttonPin1) == LOW) {x + 300;}else if (digitalRead(buttonPin2) == LOW) {} else if (digitalRead(buttonPin3) == LOW) {x - 300;}
+  }
+  Stato = 2;
+}
+void Start()
+{
+  lcd.clear();
+  lcd.setCursor(5, 0); lcd.print("select"); lcd.setCursor(3, 2); lcd.print("difficulty");
+  delay(500);
+  BottoniDif();
 }
 
-void loop() {}
+void Gioco() {
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("START");
+  delay(500);
+}
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(buttonPin3, INPUT_PULLUP);
+  Stato = 1;
+  lcd.setCursor(2, 0); lcd.print("Welcome in"); lcd.setCursor(1, 1);
+  lcd.print("Press the Face"); delay(3000); lcd.clear();
+
+
+}
+
+void loop() {
+  if (Stato == 1) {
+    Start();
+  }
+  if (Stato == 2) {
+    Gioco();
+  }
+}
